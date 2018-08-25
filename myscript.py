15e595e7ca00edbffd4983480da5660553a230fa
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-08-25 11:03:43 -0400

    read csv, op data types

--- myscript.py
+++ myscript.py
@@ -1,11 +1,25 @@
 import sys
+import pandas
+
+# This program will take arguments:
+# slide_name
+# user_name
+# tile_size
+# db_host
 
 program_name = sys.argv[0]
 arguments = sys.argv[1:]
 count = len(arguments)
-print count
 
-if count != 4 :
-    print "Usage: python " + program_name + " slide_name user_name tile_size"
-    sys.exit (1)
+# check command-line arguments
+if count != 5:
+    print 'Usage: python ' + program_name + ' slide_name user_name tile_size db_host'
+    # sys.exit(1)
 
+# temporarily using 1 input file
+filename = 'input_demo.csv'
+df = pandas.read_csv(filename)
+
+for name, values in df.iteritems():
+    print '{name}: {type}'.format(name=name, type=type(values[0]))
+    # print '{name}: {value}'.format(name=name, value=values[0])
