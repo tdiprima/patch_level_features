6544079d0fe43757083091464b3abf3188d9c4a2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-08-25 11:26:18 -0400

    dtypes list and readfile_demo

--- myscript.py
+++ myscript.py
@@ -17,22 +17,27 @@ if count != 5:
     print 'Usage: python ' + program_name + ' slide_name user_name tile_size db_host'
     # sys.exit(1)
 
-# temporarily using 1 input file
+
+def readfile_demo(filename):
+    # temporarily using 1 input file
+    df = pandas.read_csv(filename)
+
+    for name, values in df.iteritems():
+        print '{name}: {type}'.format(name=name, type=type(values[0]))
+        # print '{name}: {value}'.format(name=name, value=values[0])
+
+    # Convert Polygon string to array of float values
+    str1 = df['Polygon'][0]
+
+    if str1.startswith('[') and str1.endswith(']'):
+        # slice first and last
+        str2 = str1[1:-1]
+
+    print str2
+
+    arr = np.fromstring(str2, dtype=float, sep=':')
+    print arr
+
+
 filename = 'input_demo.csv'
-df = pandas.read_csv(filename)
-
-for name, values in df.iteritems():
-    print '{name}: {type}'.format(name=name, type=type(values[0]))
-    # print '{name}: {value}'.format(name=name, value=values[0])
-
-# Convert Polygon string to array of float values
-str1 = df['Polygon'][0]
-
-if str1.startswith('[') and str1.endswith(']'):
-    # slice first and last
-    str2 = str1[1:-1]
-
-print str2
-
-arr = np.fromstring(str2, dtype=float, sep=':')
-print arr
+readfile_demo(filename)
