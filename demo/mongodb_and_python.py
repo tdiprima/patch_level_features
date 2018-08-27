d2e8c7e75825e451aa2c9e36a7d1bf339e02c57a
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-08-27 12:08:24 -0400

    mongodb and python

---
+++ demo/mongodb_and_python.py
@@ -0,0 +1,12 @@
+# https://realpython.com/introduction-to-mongodb-and-python/
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.mydb
+coll = db.objects
+query = {}
+# items = coll.find_one(query)
+# print(items)
+items = coll.find(query)
+for item in items:
+    print(item)
