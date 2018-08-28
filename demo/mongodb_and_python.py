10cc28a7b728450cf5cb6abdf71b5439b8238305
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-08-28 13:12:32 -0400

    close client

--- demo/mongodb_and_python.py
+++ demo/mongodb_and_python.py
@@ -5,8 +5,12 @@ client = MongoClient('mongodb://localhost:27017')
 db = client.mydb
 coll = db.objects
 query = {}
+
 # items = coll.find_one(query)
 # print(items)
+
 items = coll.find(query)
 for item in items:
     print(item)
+
+client.close()
