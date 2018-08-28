c4fb4ecf092020e4247bfd0c9e20706e963914f5
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-08-28 13:28:58 -0400

    connection check

--- demo/mongodb_and_python.py
+++ demo/mongodb_and_python.py
@@ -1,16 +1,32 @@
 # https://realpython.com/introduction-to-mongodb-and-python/
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
-client = MongoClient('mongodb://localhost:27017')
-db = client.mydb
-coll = db.objects
-query = {}
 
-# items = coll.find_one(query)
-# print(items)
+def mongodb_connect(client_uri):
+    try:
+        return MongoClient(client_uri, serverSelectionTimeoutMS=1)
+    except errors.ConnectionFailure:
+        print("Failed to connect to server {}".format(client_uri))
+        exit(1)
 
-items = coll.find(query)
-for item in items:
-    print(item)
 
-client.close()
+try:
+    client = mongodb_connect('mongodb://localhost:27017')
+    client.server_info()  # force connection, trigger error to be caught
+
+    db = client.mydb
+    coll = db.objects
+    query = {}
+
+    # items = coll.find_one(query)
+    # print(items)
+
+    items = coll.find(query)
+    for item in items:
+        print(item)
+
+    client.close()
+
+except errors.ServerSelectionTimeoutError as err:
+    print(err)
+    exit(1)
