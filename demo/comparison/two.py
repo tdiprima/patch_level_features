f3d19a7e91ed3655cb0eead1e5076be74cfa040a
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-09-18 15:34:06 -0400

    wip

---
+++ demo/comparison/two.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df1 = pd.DataFrame({'Column1': [1, 2, 3, 4, 5],
+                    'Column2': ['a', 'b', 'c', 'd', 'e'],
+                    'Column3': ['r', 'u', 'k', 'j', 'f']})
+
+df2 = pd.DataFrame({'Column1': [1, 1, 1, 2, 2, 3, 3], 'ColumnB': ['a', 'd', 'e', 'r', 'w', 'y', 'h']})
+
+dfs = pd.DataFrame({})
+for name, group in df2.groupby('Column1'):
+    buffer_df = pd.DataFrame({'Column1': group['Column1'][:1]})
+    i = 0
+    for index, value in group['ColumnB'].iteritems():
+        i += 1
+        string = 'Column_' + str(i)
+        buffer_df[string] = value
+
+    dfs = dfs.append(buffer_df)
+
+result = pd.merge(df1, dfs, how='left', on='Column1')
+print(result)
