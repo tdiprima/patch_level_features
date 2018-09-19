b672478a670afb3aaf4856623eab916ed9445d5d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-09-19 16:08:53 -0400

    wip

--- demo/mongo/mongodb_and_python.py
+++ demo/mongo/mongodb_and_python.py
@@ -1,5 +1,8 @@
 # https://realpython.com/introduction-to-mongodb-and-python/
 from pymongo import MongoClient, errors
+import datetime
+
+DB_HOST = 'mongodb://localhost:27017'
 
 
 def mongodb_connect(client_uri):
@@ -11,7 +14,7 @@ def mongodb_connect(client_uri):
 
 
 try:
-    client = mongodb_connect('mongodb://localhost:27017')
+    client = mongodb_connect(DB_HOST)
     client.server_info()  # force connection, trigger error to be caught
 
     db = client.mydb
@@ -30,3 +33,20 @@ try:
 except errors.ServerSelectionTimeoutError as err:
     print(err)
     exit(1)
+
+
+def test_db():
+    try:
+        name = 'test'
+        client = mongodb_connect('mongodb://' + DB_HOST + ':27017')
+        client.server_info()  # force connection, trigger error to be caught
+        db = client.quip_comp
+        collection_saved = db[name + '_features_td']  # name
+        # patch_feature_data = collections.OrderedDict()
+        patch_feature_data = {}
+        patch_feature_data['test'] = 'test'
+        patch_feature_data['datetime'] = datetime.now()
+        collection_saved.insert_one(patch_feature_data)
+    except Exception as e:
+        print('test_db: ', e)
+        exit(1)
