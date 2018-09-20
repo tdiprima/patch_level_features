002cfc0c04e6d71ea68bf1a6f4c5ad17a1807dca
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-09-20 10:45:27 -0400

    wip

--- demo/mongo/mon.py
+++ demo/mongo/mon.py
@@ -1,13 +1,16 @@
 # Compare the patch [and patient level] features from Bridge's implementation
 # with those from Tammy’s implementation [on a subset of image tiles]
 import csv
+import numpy as np
 
 from pymongo import MongoClient
 
 # RUN PGM FOR ONE CASE_ID
 case_id = 'PC_058_0_1'
 patch_size = '512'
-db_host = ''
+db_host = 'quip3.bmi.stonybrook.edu'
+input_collection = 'test1_features_td'
+output_file = 'output1.csv'
 
 
 def is_number(s):
@@ -26,16 +29,16 @@ def get_data():
     client = MongoClient(db_host)
     db = client.quip_comp
     bridge = db.patch_level_features
-    me = db.test1_features_td
+    me = db[input_collection]
 
     my_data = []
     fields = ['case_id', 'patch_size', 'patch_min_x_pixel', 'patch_min_y_pixel', 'patch_polygon_area',
               'percent_nuclear_material', 'grayscale_patch_mean', 'grayscale_patch_std',
               'hematoxylin_patch_mean', 'hematoxylin_patch_std', 'flatness_segment_mean', 'flatness_segment_std',
-              'perimeter_segment_mean', 'perimeter_segment_std', 'circularity_segment_mean',
-              'circularity_segment_std', 'elongation_segment_mean', 'elongation_segment_std',
-              'r_GradientMean_segment_mean', 'r_GradientMean_segment_std', 'b_GradientMean_segment_mean',
-              'b_GradientMean_segment_std', 'r_cytoIntensityMean_segment_mean', 'r_cytoIntensityMean_segment_std',
+              'perimeter_segment_mean', 'perimeter_segment_std', 'circularity_segment_mean', 'circularity_segment_std',
+              'elongation_segment_mean', 'elongation_segment_std', 'r_GradientMean_segment_mean',
+              'r_GradientMean_segment_std', 'b_GradientMean_segment_mean', 'b_GradientMean_segment_std',
+              'r_cytoIntensityMean_segment_mean', 'r_cytoIntensityMean_segment_std',
               'b_cytoIntensityMean_segment_mean', 'b_cytoIntensityMean_segment_std']
     my_data.append(fields)
 
@@ -58,16 +61,17 @@ def get_data():
                 if is_number(val1) and is_number(val2):
                     my_float = abs(val1 - val2)
                     # my_float = round(float(my_float), 7)
-                    if is_number(my_float):
-                        row_vals.append(my_float)
+                    if np.isnan(my_float) or not is_number(my_float):
+                        print('is_number()', val1, val2, my_float)
+                        print("Is NaN : ", np.isnan(my_float), "\n")
                     else:
-                        print('NaN', val1, val2, my_float)
+                        row_vals.append(my_float)
 
             my_data.append(row_vals)
 
     client.close()
 
-    my_file = open('output.csv', 'a')
+    my_file = open(output_file, 'w')
     with my_file:
         writer = csv.writer(my_file)
         writer.writerows(my_data)
@@ -150,6 +154,6 @@ def example4():
         writer.writerow({'Grade': 'A', 'first_name': 'Rachael',
                          'last_name': 'Rodriguez'})
         writer.writerow({'Grade': 'B', 'first_name': 'Jane', 'last_name': 'Oscar'})
-        writer.writerow({'Grade': 'B', 'first_name': 'Jane', 'last_name': 'Loive'})
+        writer.writerow({'Grade': 'B', 'first_name': 'Jane', 'last_name': 'Love'})
 
     print("Writing complete")
